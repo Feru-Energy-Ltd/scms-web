f4689f73a660f4b2fa5b46a9d833bb9a9a104b9c
ceelogre <sam.nishimwe@yahoo.com>
2026-03-17 19:55:38 +0200

    feat: implement four oh four

---
+++ src/app/utils/constants/colors.ts
@@ -0,0 +1,14 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export const colorTheme = {
+  light: {
+    text: '#000',
+    background: '#fff',
+    tint: tintColorLight,
+  },
+  dark: {
+    text: '#fff',
+    background: '#000',
+  },
+};
